e48fd31bb7fd97660b68901eadacf26472321e38
wogusqkr0515 <wwogusqkr0515@naver.com>
2024-03-11 19:14:27 +0900

    Boj1707 BFS
    한 번 더 풀어볼 것

--- boj1707.cpp
+++ boj1707.cpp
@@ -1,87 +1,60 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <cstring>
 #include <string>
-#include <cstdlib>
+#include <algorithm>
 using namespace std;
 
+
 int K, V, E;
-string ans;
+int color[20001];
 vector<int> graph[20001];
-int visited[20001];
 
-struct Pair {
-    int n, s;
-    Pair(int num, int step): n(num), s(step) {};
-};
+bool sol() {
+    fill(color, color+V+1, -1);
 
-void dfs(int num, int step) {
-    if (ans == "NO") return;
-    visited[num] = step+1;
-    for (int i=0;i<graph[num].size();i++) {
-        // cout << "idjust num " << graph[num][i] << endl;
-        if (visited[graph[num][i]] == -1) {
-            dfs(graph[num][i], step+1);
-        }
-        else if (abs(visited[graph[num][i]] - visited[num]) > 1) {
-            // cout << "Check" << endl; 
-            ans = "NO";
-            return; 
-        }
-    }
-    return;
-}
+    for (int i=1;i<=V;i++) {
+        if (color[i] != -1) continue;
 
-string bfs() {
-    queue<int> q;
-    q.push(1);
-    visited[1] = 0;
+        queue<int> q;
+        q.push(i);
+        color[i] = 0;
 
-    while(!q.empty()) {
-        int curN = q.front(); q.pop();
-        cout << "cur num " << curN << endl;
-        for (int i=0;i<graph[curN].size();i++) {
-            cout << "idjust num " << graph[curN][i] << endl;
-            if (visited[graph[curN][i]] == -1) {
-                q.push(graph[curN][i]);
-                visited[graph[curN][i]] = visited[curN] + 1;
-            }
-            else if (abs(visited[graph[curN][i]] - visited[curN]) > 1) {
-                cout << "Check" << endl; 
-                return "NO"; 
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for (int j=0;j<graph[cur].size();j++) {
+                int nxt = graph[cur][j];
+                if (color[nxt] != -1) {
+                    if (color[nxt] == color[cur]) return false;
+                    else continue;
+                }
+                color[nxt] = (color[cur] + 1) %2;
+                q.push(nxt);
             }
         }
     }
-    return "YES";
+    return true;
 }
 
 int main() {
-    int u,v;
-    vector<string> answer;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     cin >> K;
     for (int i=0;i<K;i++) {
         cin >> V >> E;
         for (int j=0;j<E;j++) {
-            cin >> u >> v;
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+            int a, b;
+            cin >> a >> b;
+            graph[a].push_back(b);
+            graph[b].push_back(a);
         }
 
-        memset(visited, -1, sizeof(visited));
-        ans = "YES";
+        if (sol()) cout << "YES\n";
+        else cout << "NO\n";
+
         for (int j=1;j<=V;j++) {
-            if (visited[j] == -1) dfs(j, -1);
-        }
-        answer.push_back(ans);
-
-        for (int j=0;j<V;j++) {
             graph[j].clear();
         }
-    }
-
-    for (int i=0;i<K;i++) {
-        cout << answer[i] << endl;
     }
 }
