aa8c53f3d8299b4343bd6fda716f1263f256fa13
wogusqkr0515 <wwogusqkr0515@naver.com>
2024-03-11 16:15:08 +0900

    Boj11659 DP

--- boj11659.cpp
+++ boj11659.cpp
@@ -15,11 +15,11 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    snum[0] = 0;
     cin >> N >> M;
     for (int i=1;i<=N;i++) {
         cin >> nums[i];
-        if (i==1) snum[i] = nums[i];
-        else snum[i] = snum[i-1] + nums[i];
+        snum[i] = snum[i-1] + nums[i];
     }
 
     for (int i=0;i<M;i++) {
