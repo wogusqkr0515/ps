09f1fd369f85f3bc3fa79f43ab7b90aeb02523ae
wogusqkr0515 <wwogusqkr0515@naver.com>
2024-03-11 16:11:34 +0900

    Boj11659 DP

---
+++ boj11659.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int nums[100001];
+int snum[100001];
+
+void dp(int i, int j) {
+    cout << snum[j] - snum[i-1] << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    for (int i=1;i<=N;i++) {
+        cin >> nums[i];
+        if (i==1) snum[i] = nums[i];
+        else snum[i] = snum[i-1] + nums[i];
+    }
+
+    for (int i=0;i<M;i++) {
+        int l,h;
+        cin >> l >> h;
+        dp(l, h);
+    }
+}
