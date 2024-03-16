0670f2f764b58245fdea787bc431421d4c243592
wogusqkr0515 <wwogusqkr0515@naver.com>
2024-03-17 00:17:36 +0900

    Boj6581 Implemetation

--- boj6581.cpp
+++ boj6581.cpp
@@ -8,23 +8,11 @@ using namespace std;
 string br, hr;
 vector<string> v;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    string inp, pre;
+void sol() {
     int cnt = 0;
-    br = "<br>";
-    hr = "<hr>";
-    while (true) {
-        cin >> inp;
-        // if (inp == "<<") break;
-        if (cin.eof() == true) break;
-        v.push_back(inp);
-    }
-
+    
     for (int i=0;i<v.size();i++) {
-        if (v[i] == br && pre != br) {
+        if (v[i] == br) {
             cout << '\n';
             cnt = 0;
             continue;
@@ -51,4 +39,22 @@ int main() {
             cnt += v[i].length();
         }
     }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string inp;
+    
+    br = "<br>";
+    hr = "<hr>";
+    while (true) {
+        cin >> inp;
+        // if (inp == "<<") break;
+        if (cin.eof() == true) break;
+        v.push_back(inp);
+    }
+
+    sol();
 }
