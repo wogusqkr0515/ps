a8560aad1f148fc43b98e73e4fe1cf0643279360
wogusqkr0515 <wwogusqkr0515@naver.com>
2024-03-19 17:22:33 +0900

    Boj2133 DP

---
+++ boj2133.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int answer[35];
+
+void dp() {
+    if (N%2!=0) { cout << 0; return; }
+    // int i = 4;
+    answer[0] = 1; answer[2] = 3; 
+    for (int i=4;i<=N;i+=2) {
+        answer[i] += answer[i-2] * 3;
+        for (int j= i-4;j>=0;j-=2)
+            answer[i] += answer[j] * 2;
+    }
+    cout << answer[N];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+
+    dp();
+}
