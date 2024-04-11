917a048525269f4a2b3acf19a8ce755a73bb2f9c
wogusqkr0515 <wogusqkr0515@gmail.com>
2024-04-11 19:41:37 +0900

    Boj1197 Prim

--- boj1197.cpp
+++ boj1197.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
+#define X first
+#define Y second
+
 int V,E;
 int unf[10001];
+vector<pair<int, int>> adj[10001];
 tuple<int, int, int> edges[100001];
 
 int find(int x) {
@@ -23,7 +28,7 @@ bool isDiff(int a, int b) {
     return true;
 }
 
-void sol() {
+void kruskal() {
     int ans=0, cnt=0, idx=0;
     sort(edges, edges+E);
 
@@ -41,6 +46,38 @@ void sol() {
     cout << ans;
 }
 
+void prim() {
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+    bool chk[10002];
+
+    fill (chk, chk+V+1, false);
+
+    int a,b,c, ans=0, cnt=0;
+    
+    chk[1] = true;
+    for (auto nxt : adj[1])
+        pq.push({nxt.X, 1, nxt.Y});
+
+    while (!pq.empty()) {
+        tie(c,a,b) = pq.top(); pq.pop();
+
+        if (cnt==V-1) break;
+        if (chk[b]) continue;
+        chk[b] = true;
+
+        ans += c;
+        cnt++;
+
+        for (int i=0;i<adj[b].size();i++) {
+            auto nxt = adj[b][i];
+            if (chk[nxt.Y]) continue;
+            pq.push({nxt.X, b, nxt.Y});
+        }
+    }
+
+    cout << ans;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -50,7 +87,12 @@ int main() {
         int a,b,c;
         cin >> a >> b >> c;
         edges[i] = {c,a,b};
+        adj[a].push_back({c,b});
+        adj[b].push_back({c,a});
     }
 
-    sol();
+    // 크루스칼 알고리즘
+    // kruskal();
+    // 프림 알고리즘
+    prim();
 }
